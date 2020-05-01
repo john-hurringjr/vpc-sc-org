0297bebf5bde5bde262fa83e07158e46f89f17a5
John Hurring Jr <jhurring@google.com>
2020-05-01 12:04:15 -0400

    Added output for non prod shared vpc

--- 03-shared-services-projects/outputs.tf
+++ 03-shared-services-projects/outputs.tf
@@ -19,3 +19,7 @@
 output "shared_vpc_prod_project_id" {
   value = module.shared_vpc_host_project_prod.project_id
 }
+
+output "shared_vpc_non_prod_project_id" {
+  value = module.shared_vpc_host_project_non_prod.project_id
+}
