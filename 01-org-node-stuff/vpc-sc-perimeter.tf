015dd9ce96109bbe44eef498652b93f438929364
John Hurring Jr <jhurring@google.com>
2020-05-04 12:11:14 -0400

    Added in access level for service accounts for tf in 01

--- 01-org-node-stuff/vpc-sc-perimeter.tf
+++ 01-org-node-stuff/vpc-sc-perimeter.tf
@@ -89,10 +89,10 @@ resource "google_access_context_manager_service_perimeter" "service_perimeter_co
     ignore_changes = [status[0].resources]
   }
 
-  # Below will need to be added later after you have deployed all necessary projects it will be referencing and
+  # Below will need to be added later after you have deployed  necessary projects it will be referencing and
   # created the necessary access levels
-//  access_levels = [
-//
-//  ]
+  access_levels = [
+    google_access_context_manager_access_level.allow_all_tf_service_accounts.id
+  ]
 
 }
