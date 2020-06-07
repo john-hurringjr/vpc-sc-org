f8d5e32e0f72798b068abe5642b7ed4730f9cc72
John Hurring Jr <jhurring@google.com>
2020-06-07 13:10:56 -0400

    Deploying non prod for testing

--- 02-folder-structure-and-policies/folders-org-policies.tf
+++ 02-folder-structure-and-policies/folders-org-policies.tf
@@ -50,34 +50,34 @@ resource "google_folder_organization_policy" "non_prod_restrict_vpc_host" {
  *****************************************/
 # Restrict BU 1 to only get GCE images from specific project
 
-# Uncomment after Service Projects Created
-resource "google_folder_organization_policy" "prod_bu_1_folder_trusted_image_project" {
-  constraint  = "constraints/compute.trustedImageProjects"
-  folder      = google_folder.production_bu_1.id
-  list_policy {
-    allow {
-      values = [
-        "projects/${data.terraform_remote_state.rs03_shared_services_projects.outputs.os_images_prod_project_id}",
-      ]
-    }
-  }
-}
-
-# Trusted Images can be the publicly available images
-resource "google_folder_organization_policy" "non_prod_bu_1_folder_trusted_image_project" {
-  constraint  = "constraints/compute.trustedImageProjects"
-  folder      = google_folder.non_production_bu_1.id
-  list_policy {
-    allow {
-      values = [
-      "projects/cos-cloud",
-      "projects/debian-cloud",
-      "projects/centos-cloud",
-      "projects/coreos-cloud",
-      ]
-    }
-  }
-}
+//# Uncomment after Service Projects Created
+//resource "google_folder_organization_policy" "prod_bu_1_folder_trusted_image_project" {
+//  constraint  = "constraints/compute.trustedImageProjects"
+//  folder      = google_folder.production_bu_1.id
+//  list_policy {
+//    allow {
+//      values = [
+//        "projects/${data.terraform_remote_state.rs03_shared_services_projects.outputs.os_images_prod_project_id}",
+//      ]
+//    }
+//  }
+//}
+//
+//# Trusted Images can be the publicly available images
+//resource "google_folder_organization_policy" "non_prod_bu_1_folder_trusted_image_project" {
+//  constraint  = "constraints/compute.trustedImageProjects"
+//  folder      = google_folder.non_production_bu_1.id
+//  list_policy {
+//    allow {
+//      values = [
+//      "projects/cos-cloud",
+//      "projects/debian-cloud",
+//      "projects/centos-cloud",
+//      "projects/coreos-cloud",
+//      ]
+//    }
+//  }
+//}
 
 /******************************************
   Location Restriction
