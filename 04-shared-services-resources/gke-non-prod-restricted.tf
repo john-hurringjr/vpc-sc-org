a6c5116f42f0608933d5455ed51b6cae8dd890a6
John Hurring Jr <jhurring@google.com>
2020-06-15 16:36:06 -0400

    Taking down nodes and vpn for now

--- 04-shared-services-resources/gke-non-prod-restricted.tf
+++ 04-shared-services-resources/gke-non-prod-restricted.tf
@@ -78,70 +78,68 @@ resource "google_service_account" "gke_cluster_1_node_pool_1" {
   account_id  = "gke-node-pool-1"
 }
 
-
-/******************************************
-  GKE - Node Pool - 1
- *****************************************/
-
-resource "google_container_node_pool" "gke_cluster_1_node_pool_1" {
-  project = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_id
-  location = var.region_2
-  cluster = google_container_cluster.gke_cluster_1.name
-  node_count = 5
-
-  autoscaling {
-    max_node_count = 10
-    min_node_count = 3
-  }
-
-  upgrade_settings {
-    max_surge       = 2
-    max_unavailable = 2
-  }
-
-  management {
-    auto_repair   = true
-    auto_upgrade  = true
-  }
-
-  node_config {
-    disk_size_gb  = 25
-    disk_type     = "pd-ssd"
-    machine_type  = "n1-standard-2"
-    labels = {
-      node-pool = "test-node-pool-1"
-    }
-    service_account = google_service_account.gke_cluster_1_node_pool_1.email
-
-  }
-
-}
-
-/******************************************
-  Jump GCE VM to control GKE - Instance
- *****************************************/
-
-resource "google_compute_instance" "jump_to_control_gke" {
-  project = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_id
-  name            = "test-jump"
-  machine_type    = "n1-standard-2"
-  zone            = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-      size  = 20
-    }
-  }
-
-  network_interface {
-    subnetwork = module.restricted_non_prod_vpc_subnet_2.subnet_self_link
-  }
-
-  service_account {
-    scopes = ["cloud-platform"]
-    email = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_default_service_account
-  }
-
-}
-
+///******************************************
+//  GKE - Node Pool - 1
+// *****************************************/
+//
+//resource "google_container_node_pool" "gke_cluster_1_node_pool_1" {
+//  project = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_id
+//  location = var.region_2
+//  cluster = google_container_cluster.gke_cluster_1.name
+//  node_count = 5
+//
+//  autoscaling {
+//    max_node_count = 10
+//    min_node_count = 3
+//  }
+//
+//  upgrade_settings {
+//    max_surge       = 2
+//    max_unavailable = 2
+//  }
+//
+//  management {
+//    auto_repair   = true
+//    auto_upgrade  = true
+//  }
+//
+//  node_config {
+//    disk_size_gb  = 25
+//    disk_type     = "pd-ssd"
+//    machine_type  = "n1-standard-2"
+//    labels = {
+//      node-pool = "test-node-pool-1"
+//    }
+//    service_account = google_service_account.gke_cluster_1_node_pool_1.email
+//
+//  }
+//
+//}
+//
+///******************************************
+//  Jump GCE VM to control GKE - Instance
+// *****************************************/
+//
+//resource "google_compute_instance" "jump_to_control_gke" {
+//  project = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_id
+//  name            = "test-jump"
+//  machine_type    = "n1-standard-2"
+//  zone            = "us-central1-a"
+//
+//  boot_disk {
+//    initialize_params {
+//      image = "debian-cloud/debian-9"
+//      size  = 20
+//    }
+//  }
+//
+//  network_interface {
+//    subnetwork = module.restricted_non_prod_vpc_subnet_2.subnet_self_link
+//  }
+//
+//  service_account {
+//    scopes = ["cloud-platform"]
+//    email = data.terraform_remote_state.rs03_shared_services_projects.outputs.gke_cluster_non_prod_project_default_service_account
+//  }
+//
+//}
