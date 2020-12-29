11dcb2c47dc063201a8a86fba32150e89e8597c6
John Hurring Jr <jhurring@google.com>
2020-12-29 15:50:25 -0500

    Remove all

--- 04-shared-services-resources/declared-variables.tf
+++ 04-shared-services-resources/declared-variables.tf
@@ -27,264 +27,264 @@
   VPC Names
  *****************************************/
 
-# On Prem
-variable "private_on_prem_prod_vpc_name" {
-  default = "priv-op-prod-vpc"
-}
-variable "private_on_prem_non_prod_vpc_name" {
-  default = "priv-op-non-prod-vpc"
-}
-variable "restricted_on_prem_prod_vpc_name" {
-  default = "rest-op-prod-vpc"
-}
-variable "restricted_on_prem_non_prod_vpc_name" {
-  default = "rest-op-non-prod-vpc"
-}
-
-# Prod
-variable "private_prod_vpc_name" {
-  default = "priv-prod-vpc"
-}
-
-variable "restricted_prod_vpc_name" {
-  default = "rest-prod-vpc"
-}
-
-# Non-Prod
-variable "private_non_prod_vpc_name" {
-  default = "priv-non-prod-vpc"
-}
-
-variable "restricted_non_prod_vpc_name" {
-  default = "rest-non-prod-vpc"
-}
-
-/******************************************
-  Regions
- *****************************************/
-variable "region_1" {
-  default = "us-east4"
-}
-
-variable "region_2" {
-  default = "us-central1"
-}
-
-variable "region_3" {
-  default = "us-west4"
-}
-
-variable "region_4" {
-  default = "northamerica-northeast1"
-}
-
-/******************************************
-  Prod - Subnet - CIDR Ranges
- *****************************************/
-# Primary
-variable "prod_vpc_subnet_1_cidr_primary" {
-  default = "10.0.0.0/19"
-}
-variable "prod_vpc_subnet_2_cidr_primary" {
-  default = "10.0.32.0/19"
-}
-
-variable "prod_vpc_subnet_3_cidr_primary" {
-  default = "10.0.64.0/19"
-}
-variable "prod_vpc_subnet_4_cidr_primary" {
-  default = "10.0.96.0/19"
-}
-
-# Secondary Pod IPs
-variable "prod_vpc_subnet_1_cidr_pods" {
-  default = "10.2.0.0/18"
-}
-
-variable "prod_vpc_subnet_2_cidr_pods" {
-  default = "10.2.64.0/18"
-}
-
-variable "prod_vpc_subnet_3_cidr_pods" {
-  default = "10.2.128.0/18"
-}
-
-variable "prod_vpc_subnet_4_cidr_pods" {
-  default = "10.2.192.0/18"
-}
-
-# Secondary Services IPs
-variable "prod_vpc_subnet_1_cidr_services" {
-  default = "10.1.0.0/19"
-}
-
-variable "prod_vpc_subnet_2_cidr_services" {
-  default = "10.1.32.0/19"
-}
-
-variable "prod_vpc_subnet_3_cidr_services" {
-  default = "10.1.64.0/19"
-}
-
-variable "prod_vpc_subnet_4_cidr_services" {
-  default = "10.1.96.0/19"
-}
-
-# Logging Settings
-variable "prod_vpc_flow_log_interval" {
-  default = "INTERVAL_5_MIN"
-}
-
-variable "prod_vpc_flow_log_sampling" {
-  default =  0.8
-}
-
-/******************************************
-  Non-Prod - Subnet - CIDR Ranges
- *****************************************/
-# Primary
-variable "non_prod_vpc_subnet_1_cidr_primary" {
-  default = "172.16.0.0/19"
-}
-
-variable "non_prod_vpc_subnet_2_cidr_primary" {
-  default = "172.16.32.0/19"
-}
-
-variable "non_prod_vpc_subnet_3_cidr_primary" {
-  default = "172.16.64.0/19"
-}
-
-variable "non_prod_vpc_subnet_4_cidr_primary" {
-  default = "172.16.96.0/19"
-}
-
-# Secondary Pod IPs
-variable "non_prod_vpc_subnet_1_cidr_pods" {
-  default = "172.18.0.0/18"
-}
-
-variable "non_prod_vpc_subnet_2_cidr_pods" {
-  default = "172.18.64.0/18"
-}
-
-variable "non_prod_vpc_subnet_3_cidr_pods" {
-  default = "172.18.128.0/18"
-}
-
-variable "non_prod_vpc_subnet_4_cidr_pods" {
-  default = "172.18.192.0/18"
-}
-
-# Secondary Services IPs
-variable "non_prod_vpc_subnet_1_cidr_services" {
-  default = "172.17.0.0/19"
-}
-
-variable "non_prod_vpc_subnet_2_cidr_services" {
-  default = "172.17.32.0/19"
-}
-
-variable "non_prod_vpc_subnet_3_cidr_services" {
-  default = "172.17.64.0/19"
-}
-
-variable "non_prod_vpc_subnet_4_cidr_services" {
-  default = "172.17.96.0/19"
-}
-
-# Logging Settings
-variable "non_prod_vpc_flow_log_interval" {
-  default = "INTERVAL_15_MIN"
-}
-
-variable "non_prod_vpc_flow_log_sampling" {
-  default =  0.2
-}
-
-/******************************************
-  Prod & Non-Prod - Firewalls
- *****************************************/
-variable "prod_deny_all_egress_priority" {
-  default = 65535
-}
-variable "prod_deny_all_ingress_priority" {
-  default = 65535
-}
-
-variable "non_prod_deny_all_egress_priority" {
-  default = 65535
-}
-variable "non_prod_deny_all_ingress_priority" {
-  default = 65535
-}
-
-/******************************************
-  On Prem
- *****************************************/
-# Project ID - We are pointing to a project outside of our Org.
-variable "on_prem_project_id" {}
-
-# Subnets
-variable "on_prem_prod_vpc_subnet_1_cidr" {
-  default = "192.168.0.0/22"
-}
-variable "on_prem_prod_vpc_subnet_2_cidr" {
-  default = "192.168.4.0/22"
-}
-variable "on_prem_prod_vpc_subnet_3_cidr" {
-  default = "192.168.8.0/22"
-}
-variable "on_prem_prod_vpc_subnet_4_cidr" {
-  default = "192.168.12.0/22"
-}
-
-variable "on_prem_non_prod_vpc_subnet_1_cidr" {
-  default = "192.168.16.0/22"
-}
-variable "on_prem_non_prod_vpc_subnet_2_cidr" {
-  default = "192.168.20.0/22"
-}
-variable "on_prem_non_prod_vpc_subnet_3_cidr" {
-  default = "192.168.24.0/22"
-}
-variable "on_prem_non_prod_vpc_subnet_4_cidr" {
-  default = "192.168.28.0/22"
-}
-
-# Logging
-variable "on_prem_prod_vpc_flow_log_interval" {
-  default = "INTERVAL_5_MIN"
-}
-variable "on_prem_prod_vpc_flow_log_sampling" {
-  default = 0.8
-}
-
-# Logging
-variable "on_prem_non_prod_vpc_flow_log_interval" {
-  default = "INTERVAL_15_MIN"
-}
-variable "on_prem_non_prod_vpc_flow_log_sampling" {
-  default = 0.2
-}
-
-/******************************************
-  GKE
- *****************************************/
-variable "gke_cluster_test_1_name" {
-  default = "gke-cluster-1-test"
-}
-
-variable "gke_cluster_test_1_master_authorized_cidr_1" {
-  default = "192.168.0.0/16"
-}
-
-variable "gke_cluster_test_1_master_authorized_cidr_2" {
-  default = "172.16.0.0/12"
-}
-
-variable "gke_cluster_test_1_master_cidr" {
-  default = "172.31.254.0/28"
-}
-variable "gke_cluster_test_1_username" {}
-variable "gke_cluster_test_1_password" {}
+//# On Prem
+//variable "private_on_prem_prod_vpc_name" {
+//  default = "priv-op-prod-vpc"
+//}
+//variable "private_on_prem_non_prod_vpc_name" {
+//  default = "priv-op-non-prod-vpc"
+//}
+//variable "restricted_on_prem_prod_vpc_name" {
+//  default = "rest-op-prod-vpc"
+//}
+//variable "restricted_on_prem_non_prod_vpc_name" {
+//  default = "rest-op-non-prod-vpc"
+//}
+//
+//# Prod
+//variable "private_prod_vpc_name" {
+//  default = "priv-prod-vpc"
+//}
+//
+//variable "restricted_prod_vpc_name" {
+//  default = "rest-prod-vpc"
+//}
+//
+//# Non-Prod
+//variable "private_non_prod_vpc_name" {
+//  default = "priv-non-prod-vpc"
+//}
+//
+//variable "restricted_non_prod_vpc_name" {
+//  default = "rest-non-prod-vpc"
+//}
+//
+///******************************************
+//  Regions
+// *****************************************/
+//variable "region_1" {
+//  default = "us-east4"
+//}
+//
+//variable "region_2" {
+//  default = "us-central1"
+//}
+//
+//variable "region_3" {
+//  default = "us-west4"
+//}
+//
+//variable "region_4" {
+//  default = "northamerica-northeast1"
+//}
+//
+///******************************************
+//  Prod - Subnet - CIDR Ranges
+// *****************************************/
+//# Primary
+//variable "prod_vpc_subnet_1_cidr_primary" {
+//  default = "10.0.0.0/19"
+//}
+//variable "prod_vpc_subnet_2_cidr_primary" {
+//  default = "10.0.32.0/19"
+//}
+//
+//variable "prod_vpc_subnet_3_cidr_primary" {
+//  default = "10.0.64.0/19"
+//}
+//variable "prod_vpc_subnet_4_cidr_primary" {
+//  default = "10.0.96.0/19"
+//}
+//
+//# Secondary Pod IPs
+//variable "prod_vpc_subnet_1_cidr_pods" {
+//  default = "10.2.0.0/18"
+//}
+//
+//variable "prod_vpc_subnet_2_cidr_pods" {
+//  default = "10.2.64.0/18"
+//}
+//
+//variable "prod_vpc_subnet_3_cidr_pods" {
+//  default = "10.2.128.0/18"
+//}
+//
+//variable "prod_vpc_subnet_4_cidr_pods" {
+//  default = "10.2.192.0/18"
+//}
+//
+//# Secondary Services IPs
+//variable "prod_vpc_subnet_1_cidr_services" {
+//  default = "10.1.0.0/19"
+//}
+//
+//variable "prod_vpc_subnet_2_cidr_services" {
+//  default = "10.1.32.0/19"
+//}
+//
+//variable "prod_vpc_subnet_3_cidr_services" {
+//  default = "10.1.64.0/19"
+//}
+//
+//variable "prod_vpc_subnet_4_cidr_services" {
+//  default = "10.1.96.0/19"
+//}
+//
+//# Logging Settings
+//variable "prod_vpc_flow_log_interval" {
+//  default = "INTERVAL_5_MIN"
+//}
+//
+//variable "prod_vpc_flow_log_sampling" {
+//  default =  0.8
+//}
+//
+///******************************************
+//  Non-Prod - Subnet - CIDR Ranges
+// *****************************************/
+//# Primary
+//variable "non_prod_vpc_subnet_1_cidr_primary" {
+//  default = "172.16.0.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_2_cidr_primary" {
+//  default = "172.16.32.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_3_cidr_primary" {
+//  default = "172.16.64.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_4_cidr_primary" {
+//  default = "172.16.96.0/19"
+//}
+//
+//# Secondary Pod IPs
+//variable "non_prod_vpc_subnet_1_cidr_pods" {
+//  default = "172.18.0.0/18"
+//}
+//
+//variable "non_prod_vpc_subnet_2_cidr_pods" {
+//  default = "172.18.64.0/18"
+//}
+//
+//variable "non_prod_vpc_subnet_3_cidr_pods" {
+//  default = "172.18.128.0/18"
+//}
+//
+//variable "non_prod_vpc_subnet_4_cidr_pods" {
+//  default = "172.18.192.0/18"
+//}
+//
+//# Secondary Services IPs
+//variable "non_prod_vpc_subnet_1_cidr_services" {
+//  default = "172.17.0.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_2_cidr_services" {
+//  default = "172.17.32.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_3_cidr_services" {
+//  default = "172.17.64.0/19"
+//}
+//
+//variable "non_prod_vpc_subnet_4_cidr_services" {
+//  default = "172.17.96.0/19"
+//}
+//
+//# Logging Settings
+//variable "non_prod_vpc_flow_log_interval" {
+//  default = "INTERVAL_15_MIN"
+//}
+//
+//variable "non_prod_vpc_flow_log_sampling" {
+//  default =  0.2
+//}
+//
+///******************************************
+//  Prod & Non-Prod - Firewalls
+// *****************************************/
+//variable "prod_deny_all_egress_priority" {
+//  default = 65535
+//}
+//variable "prod_deny_all_ingress_priority" {
+//  default = 65535
+//}
+//
+//variable "non_prod_deny_all_egress_priority" {
+//  default = 65535
+//}
+//variable "non_prod_deny_all_ingress_priority" {
+//  default = 65535
+//}
+//
+///******************************************
+//  On Prem
+// *****************************************/
+//# Project ID - We are pointing to a project outside of our Org.
+//variable "on_prem_project_id" {}
+//
+//# Subnets
+//variable "on_prem_prod_vpc_subnet_1_cidr" {
+//  default = "192.168.0.0/22"
+//}
+//variable "on_prem_prod_vpc_subnet_2_cidr" {
+//  default = "192.168.4.0/22"
+//}
+//variable "on_prem_prod_vpc_subnet_3_cidr" {
+//  default = "192.168.8.0/22"
+//}
+//variable "on_prem_prod_vpc_subnet_4_cidr" {
+//  default = "192.168.12.0/22"
+//}
+//
+//variable "on_prem_non_prod_vpc_subnet_1_cidr" {
+//  default = "192.168.16.0/22"
+//}
+//variable "on_prem_non_prod_vpc_subnet_2_cidr" {
+//  default = "192.168.20.0/22"
+//}
+//variable "on_prem_non_prod_vpc_subnet_3_cidr" {
+//  default = "192.168.24.0/22"
+//}
+//variable "on_prem_non_prod_vpc_subnet_4_cidr" {
+//  default = "192.168.28.0/22"
+//}
+//
+//# Logging
+//variable "on_prem_prod_vpc_flow_log_interval" {
+//  default = "INTERVAL_5_MIN"
+//}
+//variable "on_prem_prod_vpc_flow_log_sampling" {
+//  default = 0.8
+//}
+//
+//# Logging
+//variable "on_prem_non_prod_vpc_flow_log_interval" {
+//  default = "INTERVAL_15_MIN"
+//}
+//variable "on_prem_non_prod_vpc_flow_log_sampling" {
+//  default = 0.2
+//}
+//
+///******************************************
+//  GKE
+// *****************************************/
+//variable "gke_cluster_test_1_name" {
+//  default = "gke-cluster-1-test"
+//}
+//
+//variable "gke_cluster_test_1_master_authorized_cidr_1" {
+//  default = "192.168.0.0/16"
+//}
+//
+//variable "gke_cluster_test_1_master_authorized_cidr_2" {
+//  default = "172.16.0.0/12"
+//}
+//
+//variable "gke_cluster_test_1_master_cidr" {
+//  default = "172.31.254.0/28"
+//}
+//variable "gke_cluster_test_1_username" {}
+//variable "gke_cluster_test_1_password" {}
--- 04-shared-services-resources/net-restricted-dns-routing-firewalls.tf
+++ 04-shared-services-resources/net-restricted-dns-routing-firewalls.tf
@@ -15,104 +15,104 @@
 /******************************************
   Restricted Shared VPC Host - Prod - DNS & Routing
  *****************************************/
-module "restricted_prod_vpc_restricted_apis_dns" {
-  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
-  network_self_link = google_compute_network.restricted_prod_vpc.self_link
-}
-
-module "restricted_prod_vpc_restricted_apis_routing" {
-  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
-  network_self_link = google_compute_network.restricted_prod_vpc.self_link
-}
-
-/******************************************
-  Restricted Shared VPC Host - Non-Prod - DNS & Routing
- *****************************************/
-module "restricted_non_prod_vpc_restricted_apis_dns" {
-  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-}
-
-module "restricted_non_prod_vpc_restricted_apis_routing" {
-  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-}
-
-/******************************************
-  Restricted Shared VPC Host - Prod - Firewalls
- *****************************************/
-
-# Denies all egress on all VMs. Also enables logging for this FW rule.
-module "restricted_prod_vpc_firewall_deny_all_egress" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-egress-all-port-proto"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
-  network_self_link = google_compute_network.restricted_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_prod_vpc.name
-  priority          = var.prod_deny_all_egress_priority
-}
-
-# Denies all ingress on all VMs. Also enables logging for this FW rule.
-module "restricted_prod_vpc_firewall_deny_all_ingress" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-ingress-all-port-proto"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
-  network_self_link = google_compute_network.restricted_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_prod_vpc.name
-  priority          = var.prod_deny_all_ingress_priority
-}
-
-# Allows egress for all VMs to restrict apis vips
-module "restricted_prod_vpc_firewall_allow_egress_restricted_apis" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
-  network_self_link = google_compute_network.restricted_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_prod_vpc.name
-}
-
-/******************************************
-  Restricted Shared VPC Host - Non-Prod - Firewalls
- *****************************************/
-# Denies all egress on all VMs. Also enables logging for this FW rule.
-module "restricted_non_prod_vpc_firewall_deny_all_egress" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-egress-all-port-proto"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_non_prod_vpc.name
-  priority          = var.non_prod_deny_all_egress_priority
-}
-
-# Denies all ingress on all VMs. Also enables logging for this FW rule.
-module "restricted_non_prod_vpc_firewall_deny_all_ingress" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-ingress-all-port-proto"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_non_prod_vpc.name
-  priority          = var.non_prod_deny_all_ingress_priority
-}
-
-# Allows ingress on 22, 3389, 443 on all VMs from all rfc1918
-module "restricted_non_prod_vpc_firewall_allow_ingress_rfc1918_limited" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918-limited"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_non_prod_vpc.name
-}
-
-# Allows egress on 22, 3389, 443 on all VMs to rfc1918
-module "restricted_non_prod_vpc_firewall_allow_egress_rfc1918_limited" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-rfc1918-limited"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_non_prod_vpc.name
-}
-
-# Allows egress for all VMs to restrict apis vips
-module "restricted_non_prod_vpc_firewall_allow_egress_restricted_apis" {
-  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-restricted-apis"
-  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
-  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
-  network_name      = google_compute_network.restricted_non_prod_vpc.name
-}
+//module "restricted_prod_vpc_restricted_apis_dns" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
+//  network_self_link = google_compute_network.restricted_prod_vpc.self_link
+//}
+//
+//module "restricted_prod_vpc_restricted_apis_routing" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
+//  network_self_link = google_compute_network.restricted_prod_vpc.self_link
+//}
+//
+///******************************************
+//  Restricted Shared VPC Host - Non-Prod - DNS & Routing
+// *****************************************/
+//module "restricted_non_prod_vpc_restricted_apis_dns" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/dns/internal-restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//}
+//
+//module "restricted_non_prod_vpc_restricted_apis_routing" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/routing/restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//}
+//
+///******************************************
+//  Restricted Shared VPC Host - Prod - Firewalls
+// *****************************************/
+//
+//# Denies all egress on all VMs. Also enables logging for this FW rule.
+//module "restricted_prod_vpc_firewall_deny_all_egress" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-egress-all-port-proto"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
+//  network_self_link = google_compute_network.restricted_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_prod_vpc.name
+//  priority          = var.prod_deny_all_egress_priority
+//}
+//
+//# Denies all ingress on all VMs. Also enables logging for this FW rule.
+//module "restricted_prod_vpc_firewall_deny_all_ingress" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-ingress-all-port-proto"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
+//  network_self_link = google_compute_network.restricted_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_prod_vpc.name
+//  priority          = var.prod_deny_all_ingress_priority
+//}
+//
+//# Allows egress for all VMs to restrict apis vips
+//module "restricted_prod_vpc_firewall_allow_egress_restricted_apis" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_prod_project_id
+//  network_self_link = google_compute_network.restricted_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_prod_vpc.name
+//}
+//
+///******************************************
+//  Restricted Shared VPC Host - Non-Prod - Firewalls
+// *****************************************/
+//# Denies all egress on all VMs. Also enables logging for this FW rule.
+//module "restricted_non_prod_vpc_firewall_deny_all_egress" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-egress-all-port-proto"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_non_prod_vpc.name
+//  priority          = var.non_prod_deny_all_egress_priority
+//}
+//
+//# Denies all ingress on all VMs. Also enables logging for this FW rule.
+//module "restricted_non_prod_vpc_firewall_deny_all_ingress" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/deny-ingress-all-port-proto"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_non_prod_vpc.name
+//  priority          = var.non_prod_deny_all_ingress_priority
+//}
+//
+//# Allows ingress on 22, 3389, 443 on all VMs from all rfc1918
+//module "restricted_non_prod_vpc_firewall_allow_ingress_rfc1918_limited" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-ingress-rfc1918-limited"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_non_prod_vpc.name
+//}
+//
+//# Allows egress on 22, 3389, 443 on all VMs to rfc1918
+//module "restricted_non_prod_vpc_firewall_allow_egress_rfc1918_limited" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-rfc1918-limited"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_non_prod_vpc.name
+//}
+//
+//# Allows egress for all VMs to restrict apis vips
+//module "restricted_non_prod_vpc_firewall_allow_egress_restricted_apis" {
+//  source            = "github.com/john-hurringjr/test-modules/networking/firewall-rules/all/allow-egress-restricted-apis"
+//  project_id        = data.terraform_remote_state.rs03_shared_services_projects.outputs.shared_vpc_non_prod_project_id
+//  network_self_link = google_compute_network.restricted_non_prod_vpc.self_link
+//  network_name      = google_compute_network.restricted_non_prod_vpc.name
+//}
