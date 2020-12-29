a56c5f867b07e19be6f72bf743818b7ad3c5141c
John Hurring Jr <jhurring@google.com>
2020-12-29 17:03:38 -0500

    test

--- 02-folder-structure-and-policies/provider.tf
+++ 02-folder-structure-and-policies/provider.tf
@@ -14,10 +14,18 @@
  */
 
 terraform {
+  required_version = ">=0.13"
+
   required_providers {
     google = {
       source = "hashicorp/google"
       version = "~>3.51.0"
     }
+
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+
   }
+
 }
